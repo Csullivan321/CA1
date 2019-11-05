b1b836171bbb6b0111dfe8e50f46e4ffe2cfa0f6
Christopher Sullivan <S00188845@mail.itsligo.ie>
2019-11-05 16:33:19 +0000

    Add project files.

---
+++ CA1/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{ public enum Genre
+        {
+            Fiction,
+            Biography,
+            Computing,
+            Literature,
+
+        }
+    class Book:IComparable
+    {
+       
+        //properties
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Published { get; set; }
+        public int Pages { get; set; }
+        public Genre Genre { get; set; }
+
+        // constructors
+        public Genre(string title, string author, int pages, DateTime published, Genre genre )
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Pages = pages;
+            this.Published = published;
+            this.Genre = genre;
+
+
+        }
+
+       
+        // method
+        public override string ToString()
+        {
+            return string.Format("Title: {0} Author:{1} Published: {2}" +
+                "Pages: {3} Genre: {4}", Title, Author, Published, Pages, Genre);
+        }
+
+        // compare to sort books by author and title
+        public int CompareTo(object obj)
+        {
+            Book that = (Book)obj;
+
+            //if (this.Author)
+        }
+    }
+}
---
+++ CA1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // objects created
+            Book b1 = new Book("Harry Potter", "Stephen F", 600, 09 /12 /2008,"literature");
+            Book b2 = new Book("Hobbit", "James Joyce", 600, '15/12/2006', "literature");
+            Book b3 = new Book("Jango", "Fk Rowling", 600, '13/07/2008', "literature");
+            Book b4 = new Book("Steve Jobs", "Walter Isacson", 600, '08/09/2009', "literature");
+            Book b5 = new Book("C Sharp", "Stephen F", 600, '05/06/2017', "literature");
+            
+            // creating a list
+            List<Book> ReadingList = new List<Book>();
+            ReadingList.Add(b1);
+            ReadingList.Add(b2);
+            ReadingList.Add(b3);
+            ReadingList.Add(b4);
+            ReadingList.Add(b5);
+
+            //Array.Sort(ReadingList);
+            Display(ReadingList);
+            Console.WriteLine();
+            
+
+        }
+
+        static void Display(List<Book>books)
+        {
+            Console.WriteLine("{0,-10}{1,-10}{2,-10}{3,-10}{4,-10}", "Title", "Author", "Pages", "Published", "Genre");
+
+            //loop through all books
+            foreach(Book book in books)
+            {
+                Console.Write("{0,-10}{1,-10}{2,-10}{3,-10}{4,-10",book.Title, book.Author, book.Pages,
+                    book.Published, book.Genre);
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
